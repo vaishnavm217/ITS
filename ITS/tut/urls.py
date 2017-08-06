8ecb4742dd24a51aeb693016084d79032211e2cd
Vaishnav Murali <vaishnavm217>
2017-08-06 19:33:53 +0530

    T-1 done

--- ITS/tut/urls.py
+++ ITS/tut/urls.py
@@ -18,7 +18,5 @@ from django.contrib import admin
 from . import views
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-    url(r'^$',views.index),
-    url(r'^tut/',include('tut.urls'),name="tutorial")
+    url(r'^$',views.index,name="index"),
 ]
